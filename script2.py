58a79c2565c3f9465cc649bb891eea6043fd84c3
mohammedhamza harun shaikh <156484611+bamman9147@users.noreply.github.com>
2024-01-17 11:46:59 -0500

    modified both files
    deleted print from file 1
    added print to file 2

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #print lines from zen of python
 print("Namespaces are one honking great idea -- let's do more of those!")
 print("If the implementation is hard to explain, it's a bad idea.")
+print("Although never is often better than *right* now.")
